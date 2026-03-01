047f277f18fa9775a50740e663a398f03c5647d8
bankragilgunawan <bankragilgunawan@gmail.com>
2026-03-01 08:51:32 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,33 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installed.');
+const CACHE_NAME = 'SicBo Pro Analyzer';
+const ASSETS_TO_CACHE = [
+  '/97/',
+  '/97/index.html',
+  '/97/manifest.json',
+  '/97/ikon-192.png',
+  '/97/ikon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      // Menggunakan link absolut agar browser tidak bingung dengan domain custom
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-  // Biarkan kosong untuk fungsionalitas dasar PWA
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
